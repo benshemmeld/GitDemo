a4b2a963282a5969da317d01acd0552af7d4f8fa
Ben Shemmeld <bshemmeld@gmail.com>
2018-11-23 13:09:36 +1100

    Feature 1

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace GitDemo
             //TODO: Code will be added here
             Console.WriteLine("Here is a change I'm going to commit to master");
 
+            Console.WriteLine("Here is Feature 1");
+
             Console.WriteLine("Press ENTER to exit");
             Console.ReadLine();
         }
